2294a0dbe231919d4a85658eb0774c5cb9b0d665
micjik <micjik50@gmail.com>
2025-09-17 21:38:49 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Home1Component } from './home1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-home1',
+    template: '<h2>Home 1 Component</h2>',
+    standalone: false
+
+})
+
+export class Home1Component{
+    
+}
